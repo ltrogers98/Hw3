8d1774a255f9a832def83bfc41413b9b69ba4893
helanab <49204828+helanab@users.noreply.github.com>
2022-03-09 14:48:54 -0500

    JavaDoc started; spacing & variable names updated
    Lots of TODOs for the methods to get done, most intentionally left blank because I'm not 100% sure what we're doing and I don't want to get them wrong.

--- StronglyConnectedComponents.java
+++ StronglyConnectedComponents.java
@@ -7,123 +7,172 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+* Finds and displays strongly connected components along with their adjency list. It utilizes
+* DFS (depth first search) to ****INSERT HERE**** before ****INSERT HERE****.
+*
+* @author brock1hj
+* @author jagre1d
+* @author roger1lt
+* @author stron1le
+*
+* @since 03/09/2022
+*/
 public class StronglyConnectedComponents {
 	public static int[] visited;
 	public static Stack<Integer> visitedOrder = new Stack<Integer>();
 	public static int componentCount = 1;
-	public static Graph displayOpposites(Graph g) {
+	
+	/**
+	* TODO
+	*
+	* @param currentGraph TODO
+	*/
+	public static Graph displayOpposites(Graph currentGraph) {
 		String edges = "";
-		for (int i = 0; i<g.size(); i++) {
-			for (int j = 0; j<g.size(); j++) {
-				if (g.getAdjMatrix()[i][j]==1) {
-					edges=edges+j+"-"+i+",";
+		for (int i = 0; i < currentGraph.size(); i++) {
+			for (int j = 0; j < currentGraph.size(); j++) {
+				if (currentGraph.getAdjMatrix()[i][j] == 1) {
+					edges = edges + j + "-" + i + ",";
 				}
 			}
 		}
-		if (edges.length()!=0) {
-			edges=edges.substring(0,edges.length()-1);
+		if (edges.length() != 0) {
+			edges = edges.substring(0, edges.length() - 1);
 		}
-		return new Graph(g.size(),edges,true);
+		return new Graph(currentGraph.size(), edges, true);
 	}
+	
+	/**
+	* Identifies and displays the strongly connected components of 3 required graphs.
+	*
+	* @param arg any command line arguments; expecting null.
+	*/
 	public static void main(String[] arg) {
-		Graph g = new Graph(6, "0-1,1-2,2-0,2-3,1-4,2-5,4-5,5-4", true);
-		Graph g2 = new Graph(8, "0-1,1-2,1-3,2-6,3-4,4-1,4-7,6-2,7-6", true);
-		Graph g3 = new Graph(5, "1-2,2-0,3-0,4-0", true);
-		findComponents(g);
-		findComponents(g2);
-		findComponents(g3);
-		
+		Graph graph1 = new Graph(6, "0-1,1-2,2-0,2-3,1-4,2-5,4-5,5-4", true);
+		Graph graph2 = new Graph(8, "0-1,1-2,1-3,2-6,3-4,4-1,4-7,6-2,7-6", true);
+		Graph graph3 = new Graph(5, "1-2,2-0,3-0,4-0", true);
+		findComponents(graph1);
+		findComponents(graph2);
+		findComponents(graph3);
 	}
-	public static void firstDFS(Graph g, int node) {
-		visited[node]=1;
-		Integer[] adjNodes = g.getAdjVector(node);
-		for (int i = 0; i<adjNodes.length; i++) {
-			if (adjNodes[i]==1&&visited[i]==0) {
-				visited[i]=1;
-				firstDFS(g,i);
+	
+	/**
+	* TODO
+	*
+	* @param currentGraph TODO
+	* @param node TODO
+	*/
+	public static void firstDFS(Graph currentGraph, int node) {
+		visited[node] = 1;
+		Integer[] adjNodes = currentGraph.getAdjVector(node);
+		for (int i = 0; i < adjNodes.length; i++) {
+			if (adjNodes[i] == 1 && visited[i] == 0) {
+				visited[i] = 1;
+				firstDFS(currentGraph, i);
 			}
 		}
 		visitedOrder.push(node);
 	}
-	public static void firstDFS(Graph g) {
-		visited = new int[g.size()];
+	
+	/**
+	* TODO
+	*
+	* @param currentGraph TODO
+	*/
+	public static void firstDFS(Graph currentGraph) {
+		visited = new int[currentGraph.size()];
 		boolean done = false;
 		while (!done) {
 			done=true;
-			for (int j = 0; j<g.size(); j++) {
-				if (visited[j]==0) {
-					visited[j]=1;
-					firstDFS(g,j);
-					done=false;
+			for (int j = 0; j < currentGraph.size(); j++) {
+				if (visited[j] == 0) {
+					visited[j] = 1;
+					firstDFS(currentGraph, j);
+					done = false;
 					break;
 				}
 			}
-			
-			
 		}
 	}
-	public static void secondDFS(Graph g) {
-		visited = new int[g.size()];
-		componentCount=1;
+	
+	/**
+	* TODO
+	*
+	* @param currentGraph TODO
+	*/
+	public static void secondDFS(Graph currentGraph) {
+		visited = new int[currentGraph.size()];
+		componentCount = 1;
 		while (!visitedOrder.isEmpty()) {
 			int newStart = visitedOrder.pop();
-			if (visited[newStart]==0) {
-				secondDFS(g,newStart);
+			if (visited[newStart] == 0) {
+				secondDFS(currentGraph, newStart);
 				componentCount++;
 			}
-			
 		}
 	}
-	public static void secondDFS(Graph g,int node) {
-		
-		visited[node]=componentCount;
-		Integer[] adjNodes = g.getAdjVector(node);
-		for (int i = 0; i<adjNodes.length; i++) {
-			if (adjNodes[i]==1&&visited[i]==0) {
-				visited[i]=componentCount;
-				secondDFS(g,i);
+	
+	/**
+	* TODO
+	*
+	* @param currentGraph TODO
+	* @param node TODO
+	*/
+	public static void secondDFS(Graph currentGraph, int node) {
+		visited[node] = componentCount;
+		Integer[] adjNodes = currentGraph.getAdjVector(node);
+		for (int i = 0; i < adjNodes.length; i++) {
+			if (adjNodes[i] == 1 && visited[i] == 0) {
+				visited[i] = componentCount;
+				secondDFS(currentGraph, i);
 			}
 		}
 	}
-	public static void findComponents(Graph g) {
-		Graph c = displayOpposites(g);
-		firstDFS(g);
-		secondDFS(c);
+	
+	/**
+	* TODO
+	*
+	* @param currentGraph TODO
+	*/
+	public static void findComponents(Graph currentGraph) {
+		Graph oppositeGraph = displayOpposites(currentGraph);
+		firstDFS(currentGraph);
+		secondDFS(oppositeGraph);
 		System.out.println("Components:");
-		int ComponentNum = 1;
-		for (int i = 0; i<visited.length; i++) {
-			if(visited[i]>ComponentNum) {
-				ComponentNum=visited[i];
+		int componentNum = 1;
+		for (int i = 0; i < visited.length; i++) {
+			if(visited[i] > componentNum) {
+				componentNum = visited[i];
 			}
 		}
 		HashMap<Integer,ArrayList<Integer>> components = new HashMap<Integer,ArrayList<Integer>>();
-		for (int i = 0; i<ComponentNum; i++) {
-			components.put(i+1, new ArrayList<Integer>());
-			for (int j = 0; j<visited.length; j++) {
-				if (visited[j]==i+1) {
-					components.get(i+1).add(j);
+		for (int i = 0; i < componentNum; i++) {
+			components.put(i + 1, new ArrayList<Integer>());
+			for (int j = 0; j < visited.length; j++) {
+				if (visited[j] == i + 1) {
+					components.get(i + 1).add(j);
 				}
 			}
-				
 		}
 		for(Integer componentNumber : components.keySet() ){
-		    System.out.print("Component "+componentNumber+": ");
+		    System.out.print("Component " + componentNumber + ": ");
 		    ArrayList<Integer> componentNodes = components.get(componentNumber);
-		    for(int i = 0; i<componentNodes.size(); i++) {
-		    	System.out.print(componentNodes.get(i)+", ");
+		    for(int i = 0; i < componentNodes.size(); i++) {
+		    	System.out.print(componentNodes.get(i) + ", ");
 		    }
 		    System.out.println();
 		}
-		System.out.println("\n\nAdjacency Lists");
-		Integer[][] adjacentMat = g.getAdjMatrix();
-		for(Integer componentNumber:components.keySet() ) {
-			 System.out.print("Component "+componentNumber+"'s List: ");
+		System.out.println("\n\nAdjacency Lists:");
+		Integer[][] adjacentMat = currentGraph.getAdjMatrix();
+		for(Integer componentNumber: components.keySet() ) {
+			 System.out.print("Component " + componentNumber + "'s List: ");
 			 ArrayList<Integer> componentNodes = components.get(componentNumber);
 			 HashSet<Integer> adjacent = new HashSet<Integer>();
 			 for(Integer currNode : componentNodes) {
-				 for (int i = 0; i<g.size(); i++) {
-					 if(adjacentMat[currNode][i] == 1&&visited[currNode]!=visited[i]&&!adjacent.contains(visited[i])) {
-						 System.out.print(visited[i]+", ");
+				 for (int i = 0; i < currentGraph.size(); i++) {
+					 if(adjacentMat[currNode][i] == 1 && visited[currNode] != visited[i] && !adjacent.contains(visited[i])) {
+						 System.out.print(visited[i] + ", ");
 						 adjacent.add(visited[i]);
 					 }
 				 }
